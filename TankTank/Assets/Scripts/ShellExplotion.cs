f8decf72070b563941653e219f6d66a640e21935
Amila Pathirana <emailpathirana@gmail.com>
2020-10-19 12:56:05 -0500

    Changing Explotion .
    Tans not taking damages. May be due to use of physics function instesd of physics2D

--- TankTank/Assets/Scripts/ShellExplotion.cs
+++ TankTank/Assets/Scripts/ShellExplotion.cs
@@ -19,7 +19,7 @@ public class ShellExplotion : MonoBehaviour
         Destroy(gameObject, m_MaxLifeTime);  
     }
 
-    // Update is called once per frame
+   
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("Saw collider");
